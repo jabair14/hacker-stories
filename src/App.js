80750640e4ee37f9231b0fa2f7f2985824956bb2
Jabair <jabairk@gmail.com>
2021-09-23 13:18:22 -0400

    on page 17; Lists in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+// const title = "React"
+
+// const welcome = {
+//   greeting: 'Hey',
+//   title: 'React',
+// };
+
+function getTitle(title) {
+  return title;
+}
+//Practice more complex JavaScript data types
+
+function getNumbers(array) {
+  return array
+
+}
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Hello {getTitle('React')}
+        {/* {welcome.greeting} {welcome.title} */}
+        {/* everything in curly braces in JSX can be used for JavaScript expressions (function execution) */}
+     </h1>
+
+     <h2>
+       Lucky Numbers: {getNumbers([14, 21, 9, 19, 0])}
+     </h2>
+
+      <label htmlFor="search"> Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }

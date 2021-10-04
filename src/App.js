3bfbf6b580b1ec6834a1f9efbc8ddbaa8d42f597
Jabair <jabairk@gmail.com>
2021-10-04 14:00:09 -0400

    pg 44

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
 import logo from './logo.svg';
 import { useState } from 'react';
+import React from 'react';
 import './App.css';
-
-// const title = "React"
-
-// const welcome = {
-//   greeting: 'Hey',
-//   title: 'React',
-// };
-
-// function getTitle(title) {
-//   return title;
-// }
-// //Practice more complex JavaScript data types
-
-// function getNumbers(array) {
-//   return array
-
-// }
-
 
 const App = () => {
   const stories = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
-      author: "Jordan Walke",
+      author: 'Jordan Walke',
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
       title: 'Redux',
-      url: 'https://redux.js.org',
+      url: 'https://redux.js.org/',
       author: 'Dan Abramov, Andrew Clark',
       num_comments: 2,
       points: 5,
@@ -40,58 +23,55 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = useState('');
-  
-  const handleChange = event => {
-    // console.log(event.target.value);
-    setSearchTerm(event.target.value);
+  const handleSearch = event => {
+    console.log(event.target.value);
   };
-  
-  
+
   return (
     <div>
-      <h1>
-        My Hacker Stories
-        {/* Hello {getTitle('React')} */}
-        {/* {welcome.greeting} {welcome.title} */}
-        {/* everything in curly braces in JSX can be used for JavaScript expressions (function execution) */}
-     </h1>
+      <h1>My Hacker Stories</h1>
 
+      <Search onSearch={handleSearch} />
 
-      <label htmlFor="search"> Search: </label>
-      <input 
-        id="search" 
-        type="text" 
-        onChange={handleChange}
-      />
+      <hr />
 
-      <p>
-        searching for <strong>{searchTerm}</strong>
-      </p>
-
-
-      <hr/>
-      {/* render list here */}
-      <List list={stories}/>
-
+      <List list={stories} />
     </div>
   );
-}
+};
 
-// rendering component list 
-const List = (props) => 
+const Search = props => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+
+    props.onSearch(event);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+    </div>
+  );
+};
+
+const List = props =>
   props.list.map(item => (
-    <div  key={item.objectID}> 
-                <span>
-                  <a href={item.url}> {item.title} </a>
-                </span>
-      
-                <span> {item.author} </span>
-                <span> {item.num_comments} </span>
-                <span> {item.points} </span>
-                    
-              </div>
-  ))
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
+
 
   
   

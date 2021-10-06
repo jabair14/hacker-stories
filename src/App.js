c3d32e93a10cf0c3b364150819a63e7f9730ce4d
Jabair <jabairk@gmail.com>
2021-10-06 12:54:47 -0400

    useEffect pg 63

--- src/App.js
+++ src/App.js
@@ -22,10 +22,18 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('React');
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem('search') || 'React'  
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
+
+    // localStorage.setItem('search', event.target.value);
   };
 
   const searchedStories = stories.filter(story =>

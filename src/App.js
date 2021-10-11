6a4b73be3f8457d4adde46e1e750d5980239c199
Jabair <jabairk@gmail.com>
2021-10-11 14:41:00 -0400

    React Async Data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// import { promised } from 'q';
 import React from 'react';
 import './App.css';
 
@@ -33,6 +34,16 @@ const initialStories = [
     objectID: 1,
   },
 ];
+
+const getAsyncStories = () => 
+  new Promise (resolve => 
+    setTimeout(
+      
+      resolve({ data: { stories: initialStories } }),
+      2000
+    )
+  );
+
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
@@ -40,7 +51,13 @@ const App = () => {
     'React'
   );
 
-  const [stories, setStories] = React.useState(initialStories)
+  const [stories, setStories] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+    });
+  }, []);
 
   const handleRemoveStory = item => {
     const newStories = stories.filter(

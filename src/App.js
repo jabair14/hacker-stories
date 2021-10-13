df56f51b4269ef041eec7ae941ceb02f2b71d67e
Jabair <jabairk@gmail.com>
2021-10-13 13:32:43 -0400

    data re-fetching

--- src/App.js
+++ src/App.js
@@ -87,9 +87,12 @@ const App = () => {
   );
 
   React.useEffect(() => {
+
+    if(!searchTerm) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then(response => response.json())
       .then(result => {
         dispatchStories({
@@ -100,7 +103,7 @@ const App = () => {
       .catch(() => 
         dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
         );
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = item => {
     dispatchStories({
@@ -112,10 +115,6 @@ const App = () => {
   const handleSearch = event => {
     setSearchTerm(event.target.value);
   };
-
-  const searchedStories = stories.data.filter(story =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   return (
     <div>
@@ -138,7 +137,7 @@ const App = () => {
         <p>Loading ...</p>
       ) : (
         <List
-          list={searchedStories}
+          list={stories.data}
           onRemoveItem={handleRemoveStory}
         />
       )}

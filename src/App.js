22ab4ea3e2d5ebde7ec7748e9213fb6c6acfd322
Jabair <jabairk@gmail.com>
2021-10-21 15:19:31 -0400

    styling in react

--- src/App.js
+++ src/App.js
@@ -129,15 +129,17 @@ const App = () => {
 
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
+
+
 
     <SearchForm
       searchTerm={searchTerm}
       onSearchInput={handleSearchInput}
       onSearchSubmit={handleSearchSubmit}
     />
-      <hr />
+      
 
       {stories.isError && <p>Something went wrong ...</p>}
 
@@ -191,15 +193,19 @@ const List = ({ list, onRemoveItem }) =>
   ));
 
 const Item = ({ item, onRemoveItem }) => (
-  <div>
-    <span>
+  <div className="item">
+    <span style={{ width: '40%' }}>
       <a href={item.url}>{item.title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button type="button" onClick={() => onRemoveItem(item)}>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}>
+      <button 
+        type="button" 
+        onClick={() => onRemoveItem(item)}
+        className="button button_small"  
+      >
         Dismiss
       </button>
     </span>
